fc5f10a1b8888e70c88c344c2e9f743302199493
Toan Nguyen <nguyennhattoan.1993@gmail.com>
2022-09-10 08:46:03 +0700

    Create a new automation test framework

---
+++ actions/commons/GlobalConstant.java
@@ -0,0 +1,12 @@
+package commons;
+
+public class GlobalConstant {
+	//resources which are used for all object pages:
+	//1. Common variables
+	//2. User/Pass -> App
+	//3. User/Pass -> DB
+	//4. Database
+	//5. Server
+	//6. Environment
+	//7. ...
+}
